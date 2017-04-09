0b67753eda0d686b161da11f2fe48bc4efe60de4
kalosze <micholus1@gmail.com>
2017-04-09 23:38:30 +0200

    pierwszy commit

---
+++ interfejs/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace interfejs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            for(var i = 0; i <10;++i)
+                dataGrid.Items.Add(new kruwa() { Kolumna1 = "dana 1", Kolumna2 = "dana 2", Kolumna3 = "dana 3" });
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var b = new AddNewRecord();
+            b.Show();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            var a = new AdminMenu();
+            a.Show();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var b = new Logowanie();
+            b.Show();
+            this.Close();
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            var b = new Search();
+            b.Show();
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            var b = new tableControl();
+            b.Show();
+        }
+    }
+
+    public class kruwa
+    {
+        public string Kolumna1 { get; set; }
+        public string Kolumna2 { get; set; }
+        public string Kolumna3 { get; set; }
+    }
+
+
+}
